8cc20ac7d41a8723addb70d741c955166b611e49
Ash Furrow <ash@ashfurrow.com>
2018-10-27 11:59:36 -0400

    Extends noBadWordsRule with configuration.

--- tslint/noBadWordsRule.ts
+++ tslint/noBadWordsRule.ts
@@ -3,14 +3,19 @@ import * as Lint from "tslint"
 import * as utils from "tsutils"
 import * as ts from "typescript"
 
+interface Options {
+  filter: any
+}
+
 export class Rule extends Lint.Rules.AbstractRule {
   apply(sourceFile: ts.SourceFile): Lint.RuleFailure[] {
-    const filter = new Filter()
+    const { ruleArguments: list } = this.getOptions()
+    const filter = new Filter({ list } as any)
     return this.applyWithFunction(sourceFile, walk, { filter })
   }
 }
 
-function walk(ctx: Lint.WalkContext<any>) {
+function walk(ctx: Lint.WalkContext<Options>) {
   utils.forEachComment(ctx.sourceFile, (fullText, { kind, pos, end }) => {
     const comment = fullText.slice(pos, end)
     const cleanedComment = ctx.options.filter.clean(comment)
