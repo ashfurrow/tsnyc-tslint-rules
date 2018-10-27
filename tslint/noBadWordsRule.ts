290e0f6a22e033a8321cd9b9fd08b8745ed37284
Ash Furrow <ash@ashfurrow.com>
2018-10-27 11:36:01 -0400

    Adds basic rule.

---
+++ tslint/noBadWordsRule.ts
@@ -0,0 +1,8 @@
+import * as Lint from "tslint"
+import * as ts from "typescript"
+
+export class Rule extends Lint.Rules.AbstractRule {
+  apply(sourceFile: ts.SourceFile): Lint.RuleFailure[] {
+    return []
+  }
+}
