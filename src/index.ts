2fcd433a1d194d33a044b10b64f08ba76bb7d381
Ash Furrow <ash@ashfurrow.com>
2018-10-27 11:52:04 -0400

    Implements basic noBadWordsRule.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
 export default function hello() {
   console.log("Hello World") // tslint:disable-line
 }
+
+// TODO: This is a heck of a hack.
+let a = 123
+a = 456
--- tslint/noBadWordsRule.ts
+++ tslint/noBadWordsRule.ts
@@ -1,8 +1,21 @@
+import * as Filter from "bad-words"
 import * as Lint from "tslint"
+import * as utils from "tsutils"
 import * as ts from "typescript"
 
 export class Rule extends Lint.Rules.AbstractRule {
   apply(sourceFile: ts.SourceFile): Lint.RuleFailure[] {
-    return []
+    const filter = new Filter()
+    return this.applyWithFunction(sourceFile, walk, { filter })
   }
+}
+
+function walk(ctx: Lint.WalkContext<any>) {
+  utils.forEachComment(ctx.sourceFile, (fullText, { kind, pos, end }) => {
+    const comment = fullText.slice(pos, end)
+    const cleanedComment = ctx.options.filter.clean(comment)
+    if (comment !== cleanedComment) {
+      ctx.addFailure(pos, end, `Found a bad word in the following comment: \`${comment}\``)
+    }
+  })
 }
